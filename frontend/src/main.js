18f7aa037a55494778f3ace00bc689b6940cc1a0
saulrosua8 <saul.rosua.ortiz@gmail.com>
2024-12-10 09:35:59 +0100

    10-12-24

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,19 +2,81 @@ import { createSelect } from "./components/createHoursSelect/createHoursSelect";
 import { createButton } from "./components/loadButton/loadButton";
 import { createSpinner } from "./components/spinner/spinner";
 
-function handleClick() {
-    loadDataPerHour(hour) 
+
+const renderLogin = () =>{
+    
+    const app = document.getElementById("app");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = "LOGIN";
+    app.appendChild(h1);
+    
+    app.innerHTML += `
+    <div class="login-container">
+        <h2>Iniciar Sesión</h2>
+        <form id="form-login">
+            <input type="text" id="username" name="username" placeholder="Nombre de usuario" required>
+            <input type="password" id="password" name="password" placeholder="Contraseña" required>
+            <button type="submit">Entrar</button>
+        </form>
+        <a href="register.html">Registrarse</a>
+    </div>`
+    ;
+
+    document.getElementById("form-producto")
+    .addEventListener("submit", sendLogin);
+
+
+    
+
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const app= document.getElementById("app");
-    const spinner= createSpinner();
-    // app.appendChild(spinner);
-    const loadPricesButton= createButton();
-    app.appendChild(loadPricesButton);
-    loadPricesButton.addEventListener("click" , handleClick);
-    const selectHours= createSelect();
-    console.log(selectHours);
-    app.appendChild(selectHours);
+const sendLogin = (event) => {
+    //no recargar paginas
+    event.preventDefault();
+    event.target.reset();
+
+}
+
+
+
+
+
+
+renderLogin();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function handleClick() {
+//     loadDataPerHour(hour) 
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const app= document.getElementById("app");
+//     const spinner= createSpinner();
+//     // app.appendChild(spinner);
+//     const loadPricesButton= createButton();
+//     app.appendChild(loadPricesButton);
+//     loadPricesButton.addEventListener("click" , handleClick);
+//     const selectHours= createSelect();
+//     console.log(selectHours);
+//     app.appendChild(selectHours);
     
-});
+// });
