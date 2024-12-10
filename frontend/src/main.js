be176d34d586a4400b094a0b971bd1cbc28d6bad
paco <pacopedrosa2018@gmail.com>
2024-12-10 09:31:13 +0100

    modo claro y oscuro y selectMonth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import { createSelect } from "./components/createHoursSelect/createHoursSelect";
+import { createMonthSelect } from "./components/CreateMothSelect/createMothSelect.js";
 import { createButton } from "./components/loadButton/loadButton";
 import { createSpinner } from "./components/spinner/spinner";
-
+import { showButton, createToggleButton } from "./components/toggleButton/toggleButton.js";
 function handleClick() {
     loadDataPerHour(hour) 
 }
@@ -9,12 +10,17 @@ function handleClick() {
 document.addEventListener("DOMContentLoaded", () => {
     const app= document.getElementById("app");
     const spinner= createSpinner();
-    // app.appendChild(spinner);
+    //app.appendChild(spinner);
     const loadPricesButton= createButton();
     app.appendChild(loadPricesButton);
     loadPricesButton.addEventListener("click" , handleClick);
     const selectHours= createSelect();
     console.log(selectHours);
     app.appendChild(selectHours);
+    const selectMonth = createMonthSelect();
+    console.log(selectMonth);
+    app.appendChild(selectMonth);
+    createToggleButton();
+    showButton();
     
 });
